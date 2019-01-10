0aa6c65f921dad67e6424715eb865a95490e74d8
Hemin Johnson <achayan1992@gmail.com>
2019-01-10 21:25:26 +0100

    feat: add the user endpoint

--- server.js
+++ server.js
@@ -95,6 +95,17 @@ app.patch('/todos/:id', (req, res) => {
   })
 })
 
+app.post('/users', (req, res) => {
+  var body = _.pick(['email', 'password'], req.body)
+  var user = new User(body)
+
+  user.save().then((user) => {
+    res.send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
+
 app.listen(3000, () => {
   console.log('Started on port 3000')
 })
