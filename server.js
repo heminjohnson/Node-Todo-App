2683f905fbd1fafdee871a4a7f566fc772d06377
Hemin Johnson <achayan1992@gmail.com>
2019-01-13 19:39:31 +0100

    feat: make get user endpoint private

--- server.js
+++ server.js
@@ -108,6 +108,20 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.get('/users/me', (req, res) => {
+  var token = req.header('x-auth')
+
+  User.findByToken(token).then((user) => {
+    if(!user) {
+      return Promise.reject()
+    }
+
+    res.send(user)
+  }).catch((e) => {
+    res.status(401).send()
+  })
+})
+
 app.listen(3000, () => {
   console.log('Started on port 3000')
 })
