c9e1ef875726ed2db927aea55c8d217b80b2dd69
Hemin Johnson <achayan1992@gmail.com>
2019-01-13 19:49:55 +0100

    refactor: refactor authenticate to a middleware

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var _ = require('ramda')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 
@@ -108,18 +109,8 @@ app.post('/users', (req, res) => {
   })
 })
 
-app.get('/users/me', (req, res) => {
-  var token = req.header('x-auth')
-
-  User.findByToken(token).then((user) => {
-    if(!user) {
-      return Promise.reject()
-    }
-
-    res.send(user)
-  }).catch((e) => {
-    res.status(401).send()
-  })
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(3000, () => {
