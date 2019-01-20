ebadcc0cc69ef4e87780c680c8998dad4ec7d79e
Hemin Johnson <achayan1992@gmail.com>
2019-01-20 20:11:05 +0100

    fix: remove unnecessary console logs

--- server.js
+++ server.js
@@ -126,7 +126,6 @@ app.post('/users/login', (req, res) => {
 })
 
 app.delete('/users/me/token', authenticate, (req, res) => {
-  console.log(req.token)
   req.user.removeToken(req.token).then(() => {
     res.status(200).send()
   }, () => {
