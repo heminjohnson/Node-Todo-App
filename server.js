14019874c54d48a748838dc0d8cd9e2a4d44c03f
Hemin Johnson <achayan1992@gmail.com>
2019-01-22 22:49:28 +0100

    feat: update the get todos by id to private

--- server.js
+++ server.js
@@ -37,14 +37,17 @@ app.get('/todos', authenticate, (req, res) => {
   })
 })
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id
 
   if(!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user.id
+  }).then((todo) => {
     if(!todo) {
       return res.status(404).send()
     }
