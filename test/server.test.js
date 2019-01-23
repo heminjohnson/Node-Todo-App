b6f34da2a4e22c671b070f406e129966865a8163
Hemin Johnson <achayan1992@gmail.com>
2019-01-23 22:17:38 +0100

    test: add test to not update the user when requested by other users

--- test/server.test.js
+++ test/server.test.js
@@ -190,6 +190,21 @@ describe('PATCH /todos/:id', () => {
       .end(done)
   })
 
+  it('should not update the todo requested by other user', (done) => {
+    var hexId = todos[0]._id.toHexString()
+    var text = 'This should be the new text'
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .set('x-auth', users[1].tokens[0].token)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(404)
+      .end(done)
+  })
+
   it('should clear completedAt when todo is not completed', (done) => {
     var hexId = todos[1]._id.toHexString()
     var text = 'This should be the new text!!!'
